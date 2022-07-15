4738e8e500b056efc6473f031c2d6e945af14c2c
jisu5959 <leejisoo0324-@naver.com>
2022-07-15 15:29:30 +0900

    delete fragment

--- app/src/main/java/com/example/hackathon/screen/MainActivity.kt
+++ app/src/main/java/com/example/hackathon/screen/MainActivity.kt
@@ -5,47 +5,13 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.hackathon.R
 import com.example.hackathon.databinding.ActivityMainBinding
-import com.example.hackathon.screen.one.FirstFragment
-import com.example.hackathon.screen.three.ThirdFragment
-import com.example.hackathon.screen.two.SecondFragment
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-
-    private val mainFragment1 by lazy { FirstFragment() }
-    private val mainFragment2 by lazy { SecondFragment() }
-    private val mainFragment3 by lazy { ThirdFragment() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        changeFragment(mainFragment2)
-        binding.bottomView.selectedItemId = R.id.two
-        binding.bottomView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.one ->{
-                    changeFragment(mainFragment1)
-                    true
-                }
-                R.id.two ->{
-                    changeFragment(mainFragment2)
-                    true
-                }
-                R.id.three ->{
-                    changeFragment(mainFragment3)
-                    true
-                }
-                else->{
-                    changeFragment(mainFragment2)
-                    true
-                }
-            }
-        }
     }
 
-    private fun changeFragment(fragment: androidx.fragment.app.Fragment){
-        supportFragmentManager.beginTransaction().replace(R.id.frame, fragment).commit()
-    }
 }
--- app/src/main/java/com/example/hackathon/screen/SplashActivity.kt
+++ app/src/main/java/com/example/hackathon/screen/SplashActivity.kt
@@ -1,20 +1,34 @@
 package com.example.hackathon.screen
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.hackathon.R
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({ //스플래쉬
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 1500)
+
+        val pref = getSharedPreferences("isFirst", MODE_PRIVATE)
+        val first = pref.getBoolean("isFirst", false)
+        if (first == false) {
+            Log.d("Is first Time?", "first")
+            val editor = pref.edit()
+            editor.putBoolean("isFirst", true)
+            editor.commit()
+            Handler(Looper.getMainLooper()).postDelayed({ //스플래쉬
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 1500)
+            //앱 최초 실행시 하고 싶은 작업
+        } else {
+            Log.d("Is first Time?", "not first")
+        }
     }
 }
